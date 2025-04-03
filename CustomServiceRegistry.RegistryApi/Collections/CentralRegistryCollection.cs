918198b903890bd2222a2df83b625331f74ba499
Linn-Thit-Htoo <lth1212001@gmail.com>
2025-04-03 16:51:31 +0630

    -

--- CustomServiceRegistry.RegistryApi/Collections/CentralRegistryCollection.cs
+++ CustomServiceRegistry.RegistryApi/Collections/CentralRegistryCollection.cs
@@ -1,24 +1,23 @@
 ﻿using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson;
 
-namespace CustomServiceRegistry.RegistryApi.Collections
+namespace CustomServiceRegistry.RegistryApi.Collections;
+
+public class CentralRegistryCollection
 {
-    public class CentralRegistryCollection
-    {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; }
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
 
-        [BsonRepresentation(BsonType.String)]
-        public Guid ServiceId { get; set; }
+    [BsonRepresentation(BsonType.String)]
+    public Guid ServiceId { get; set; }
 
-        [BsonRepresentation(BsonType.String)]
-        public Guid TenantId { get; set; }
+    [BsonRepresentation(BsonType.String)]
+    public Guid TenantId { get; set; }
 
-        public string ServiceName { get; set; }
-        public string Scheme { get; set; }
-        public string HostName { get; set; }
-        public int Port { get; set; }
-        public string HealthCheckUrl { get; set; }
-    }
+    public string ServiceName { get; set; }
+    public string Scheme { get; set; }
+    public string HostName { get; set; }
+    public int Port { get; set; }
+    public string HealthCheckUrl { get; set; }
 }
