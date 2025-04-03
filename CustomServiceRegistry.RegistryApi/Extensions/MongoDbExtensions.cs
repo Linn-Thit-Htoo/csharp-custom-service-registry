b9072faff63ef9c3ddde43e8e93e3d394d31884e
Linn Thit Htoo <lth1212001@gmail.com>
2025-04-03 20:13:49 +0630

    chore: changed connection string

--- CustomServiceRegistry.RegistryApi/Extensions/MongoDbExtensions.cs
+++ CustomServiceRegistry.RegistryApi/Extensions/MongoDbExtensions.cs
@@ -7,7 +7,7 @@ namespace CustomServiceRegistry.RegistryApi.Extensions
         public static IMongoCollection<T> GetCollection<T>(this string collectionName)
         {
             var client = new MongoClient(
-                "mongodb+srv://mglinnthithtoo:IVIxNP41oHNh6rq3@cluster1.gwvyz.mongodb.net/"
+                "mongodb://localhost:27017"
             );
             var database = client.GetDatabase("ServiceRegistry");
             var collection = database.GetCollection<T>(collectionName);
--- CustomServiceRegistry.RegistryApi/Services/ActiveHealthCheckBackgroundService.cs
+++ CustomServiceRegistry.RegistryApi/Services/ActiveHealthCheckBackgroundService.cs
@@ -30,63 +30,33 @@ namespace CustomServiceRegistry.RegistryApi.Services
 
         protected async override Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while (true)
+            try
             {
-                var scope = _serviceScopeFactory.CreateScope();
-                var registryService = scope.ServiceProvider.GetRequiredService<IServiceRegistryService>();
-
-                var lst = await _centralRegistryCollection.Find(x => true).ToListAsync(cancellationToken: stoppingToken);
-
-                if (lst is not null && lst.Count >0)
+                while (true)
                 {
-                    foreach (var item in lst)
+                    var scope = _serviceScopeFactory.CreateScope();
+                    var registryService = scope.ServiceProvider.GetRequiredService<IServiceRegistryService>();
+
+                    var lst = await _centralRegistryCollection.Find(x => true).ToListAsync(cancellationToken: stoppingToken);
+
+                    if (lst is not null && lst.Count > 0)
                     {
-                        var retryPolicy = Policy
-                        .Handle<WebException>()
-                        .Or<Exception>()
-                        .RetryAsync(
-                            _setting.RetryCount,
-                            onRetry: async (exception, retryCount, context) =>
-                            {
-                                _logger.LogError($"Service: {item.ServiceId}, Tenant Id: {item.TenantId} Retry Count: {retryCount}, Reason: {exception}");
-
-                                await _serviceLogCollection.InsertOneAsync(new ServiceLogCollection()
-                                {
-                                    LogId = Guid.NewGuid(),
-                                    IsSuccess = false,
-                                    ErrorMessage = exception.ToString(),
-                                    LogAt = DateTime.UtcNow,
-                                    TenantId = item.TenantId,
-                                    ServiceInfo = new CentralRegistryCollection()
-                                    {
-                                        ServiceId = item.ServiceId,
-                                        HealthCheckUrl = item.HealthCheckUrl,
-                                        HostName = item.HostName,
-                                        Port = item.Port,
-                                        Id = item.Id,
-                                        Scheme = item.Scheme,
-                                        ServiceName = item.ServiceName,
-                                        TenantId = item.TenantId
-                                    }
-                                }, cancellationToken: stoppingToken);
-                            }
-                        );
-
-                        var fallbackPolicy = Policy<HttpResponseMessage>
+                        foreach (var item in lst)
+                        {
+                            var retryPolicy = Policy
                             .Handle<WebException>()
                             .Or<Exception>()
-                            .FallbackAsync(
-                                async (ct) =>
+                            .RetryAsync(
+                                _setting.RetryCount,
+                                onRetry: async (exception, retryCount, context) =>
                                 {
-                                    _logger.LogError($"All health checks failed for service: {item.ServiceId}. Service: {item.ServiceId} will be deregistered. Tenant Id: {item.TenantId}"
-                                    );
-                                    await registryService.DeregisterAsync(item.ServiceId, ct);
+                                    _logger.LogError($"Service: {item.ServiceId}, Tenant Id: {item.TenantId} Retry Count: {retryCount}, Reason: {exception}");
 
                                     await _serviceLogCollection.InsertOneAsync(new ServiceLogCollection()
                                     {
                                         LogId = Guid.NewGuid(),
                                         IsSuccess = false,
-                                        ErrorMessage = $"All health checks failed. Service Id: {item.ServiceId}, Service Name: {item.ServiceName} will be deregistered from the registry.",
+                                        ErrorMessage = exception.ToString(),
                                         LogAt = DateTime.UtcNow,
                                         TenantId = item.TenantId,
                                         ServiceInfo = new CentralRegistryCollection()
@@ -98,44 +68,81 @@ namespace CustomServiceRegistry.RegistryApi.Services
                                             Id = item.Id,
                                             Scheme = item.Scheme,
                                             ServiceName = item.ServiceName,
-                                            TenantId = item.TenantId,
+                                            TenantId = item.TenantId
                                         }
                                     }, cancellationToken: stoppingToken);
-
-                                    return new HttpResponseMessage(
-                                        HttpStatusCode.ServiceUnavailable
-                                    );
                                 }
                             );
 
-                        var policy = fallbackPolicy.WrapAsync(retryPolicy);
-                        HttpClient httpClient = scope.ServiceProvider.GetRequiredService<HttpClient>();
+                            var fallbackPolicy = Policy<HttpResponseMessage>
+                                .Handle<WebException>()
+                                .Or<Exception>()
+                                .FallbackAsync(
+                                    async (ct) =>
+                                    {
+                                        _logger.LogError($"All health checks failed for service: {item.ServiceId}. Service: {item.ServiceId} will be deregistered. Tenant Id: {item.TenantId}"
+                                        );
+                                        await registryService.DeregisterAsync(item.ServiceId, ct);
 
-                        await policy.ExecuteAsync(() => httpClient.GetAsync(item.HealthCheckUrl, cancellationToken: stoppingToken));
+                                        await _serviceLogCollection.InsertOneAsync(new ServiceLogCollection()
+                                        {
+                                            LogId = Guid.NewGuid(),
+                                            IsSuccess = false,
+                                            ErrorMessage = $"All health checks failed. Service Id: {item.ServiceId}, Service Name: {item.ServiceName} will be deregistered from the registry.",
+                                            LogAt = DateTime.UtcNow,
+                                            TenantId = item.TenantId,
+                                            ServiceInfo = new CentralRegistryCollection()
+                                            {
+                                                ServiceId = item.ServiceId,
+                                                HealthCheckUrl = item.HealthCheckUrl,
+                                                HostName = item.HostName,
+                                                Port = item.Port,
+                                                Id = item.Id,
+                                                Scheme = item.Scheme,
+                                                ServiceName = item.ServiceName,
+                                                TenantId = item.TenantId,
+                                            }
+                                        }, cancellationToken: stoppingToken);
 
-                        await _serviceLogCollection.InsertOneAsync(new ServiceLogCollection()
-                        {
-                            LogId = Guid.NewGuid(),
-                            IsSuccess = true,
-                            ErrorMessage = null,
-                            LogAt = DateTime.UtcNow,
-                            TenantId = item.TenantId,
-                            ServiceInfo = new CentralRegistryCollection()
+                                        return new HttpResponseMessage(
+                                            HttpStatusCode.ServiceUnavailable
+                                        );
+                                    }
+                                );
+
+                            var policy = fallbackPolicy.WrapAsync(retryPolicy);
+                            HttpClient httpClient = scope.ServiceProvider.GetRequiredService<HttpClient>();
+
+                            await policy.ExecuteAsync(() => httpClient.GetAsync(item.HealthCheckUrl, cancellationToken: stoppingToken));
+
+                            await _serviceLogCollection.InsertOneAsync(new ServiceLogCollection()
                             {
-                                ServiceId = item.ServiceId,
-                                HealthCheckUrl = item.HealthCheckUrl,
-                                HostName = item.HostName,
-                                Port = item.Port,
-                                Id = item.Id,
-                                Scheme = item.Scheme,
-                                ServiceName = item.ServiceName,
-                                TenantId = item.TenantId
-                            }
-                        }, cancellationToken: stoppingToken);
+                                LogId = Guid.NewGuid(),
+                                IsSuccess = true,
+                                ErrorMessage = null,
+                                LogAt = DateTime.UtcNow,
+                                TenantId = item.TenantId,
+                                ServiceInfo = new CentralRegistryCollection()
+                                {
+                                    ServiceId = item.ServiceId,
+                                    HealthCheckUrl = item.HealthCheckUrl,
+                                    HostName = item.HostName,
+                                    Port = item.Port,
+                                    Id = item.Id,
+                                    Scheme = item.Scheme,
+                                    ServiceName = item.ServiceName,
+                                    TenantId = item.TenantId
+                                }
+                            }, cancellationToken: stoppingToken);
 
-                        await Task.Delay(TimeSpan.FromMilliseconds(1000), stoppingToken);
+                            await Task.Delay(TimeSpan.FromMilliseconds(1000), stoppingToken);
+                        }
                     }
                 }
+            }
+            catch (Exception ex)
+            {
+                throw;
             }
         }
     }
