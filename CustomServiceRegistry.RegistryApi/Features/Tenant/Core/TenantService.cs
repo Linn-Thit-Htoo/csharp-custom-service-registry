ab8c2682a7617d78234a9fc7c770b15eac233ed8
Linn-Thit-Htoo <lth1212001@gmail.com>
2025-04-03 08:29:13 +0630

    -

--- CustomServiceRegistry.RegistryApi/Features/Tenant/Core/TenantService.cs
+++ CustomServiceRegistry.RegistryApi/Features/Tenant/Core/TenantService.cs
@@ -17,22 +17,29 @@ public class TenantService : ITenantService
         _tenantCollection = CollectionNames.TenantCollection.GetCollection<TenantCollection>();
     }
 
-    public async Task<Result<CreateTenantResponse>> CreateTenantAsync(CreateTenantCommand command, CancellationToken cs = default)
+    public async Task<Result<CreateTenantResponse>> CreateTenantAsync(
+        CreateTenantCommand command,
+        CancellationToken cs = default
+    )
     {
         var tenant = command.ToCollection();
         await _tenantCollection.InsertOneAsync(tenant, cancellationToken: cs);
 
-        return Result<CreateTenantResponse>.Success(new CreateTenantResponse()
-        {
-            ApiKey = tenant.TenantId.ToString()
-        });
+        return Result<CreateTenantResponse>.Success(
+            new CreateTenantResponse() { ApiKey = tenant.TenantId.ToString() }
+        );
     }
 
-    public async Task<Result<GetTenantByIdResponse>> GetTenantByIdAsync(string tenantId, CancellationToken cancellationToken = default)
+    public async Task<Result<GetTenantByIdResponse>> GetTenantByIdAsync(
+        string tenantId,
+        CancellationToken cancellationToken = default
+    )
     {
         Result<GetTenantByIdResponse> result;
 
-        var item = await _tenantCollection.Find(x => x.TenantId == Guid.Parse(tenantId)).FirstOrDefaultAsync(cancellationToken: cancellationToken);
+        var item = await _tenantCollection
+            .Find(x => x.TenantId == Guid.Parse(tenantId))
+            .FirstOrDefaultAsync(cancellationToken: cancellationToken);
 
         if (item is null)
         {
@@ -40,11 +47,13 @@ public class TenantService : ITenantService
             goto result;
         }
 
-        result = Result<GetTenantByIdResponse>.Success(new GetTenantByIdResponse()
-        {
-            TenantId = item.TenantId,
-            ApplicationName = item.ApplicationName
-        });
+        result = Result<GetTenantByIdResponse>.Success(
+            new GetTenantByIdResponse()
+            {
+                TenantId = item.TenantId,
+                ApplicationName = item.ApplicationName,
+            }
+        );
 
         result:
         return result;
