ced5cdcafd578f0b2887fb75be641e3c8c43d2e3
Linn Thit Htoo <lth1212001@gmail.com>
2025-04-06 00:10:07 +0630

    -

--- CustomServiceRegistry.RegistryApi/Features/ServiceRegistry/Core/IServiceRegistryService.cs
+++ CustomServiceRegistry.RegistryApi/Features/ServiceRegistry/Core/IServiceRegistryService.cs
@@ -2,12 +2,11 @@
 using CustomServiceRegistry.RegistryApi.Features.ServiceRegistry.RegisterService;
 using CustomServiceRegistry.RegistryApi.Utils;
 
-namespace CustomServiceRegistry.RegistryApi.Features.ServiceRegistry.Core
+namespace CustomServiceRegistry.RegistryApi.Features.ServiceRegistry.Core;
+
+public interface IServiceRegistryService
 {
-    public interface IServiceRegistryService
-    {
-        Task<Result<RegisterServiceResponse>> RegisterServiceAsync(RegisterServiceCommand command, Guid tenantId, CancellationToken cs = default);
-        Task<Result<DeregisterServiceResponse>> DeregisterAsync(DeregisterServiceCommand command, Guid tenantId, CancellationToken cs = default);
-        Task DeregisterAsync(Guid id, CancellationToken cs = default);
-    }
+    Task<Result<RegisterServiceResponse>> RegisterServiceAsync(RegisterServiceCommand command, Guid tenantId, CancellationToken cs = default);
+    Task<Result<DeregisterServiceResponse>> DeregisterAsync(DeregisterServiceCommand command, Guid tenantId, CancellationToken cs = default);
+    Task DeregisterAsync(Guid id, CancellationToken cs = default);
 }
