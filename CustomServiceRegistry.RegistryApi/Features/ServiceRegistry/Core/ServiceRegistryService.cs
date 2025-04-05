c2089e644300b6cc1ca02d339ae406e179100c9d
Linn Thit Htoo <lth1212001@gmail.com>
2025-04-06 00:10:53 +0630

    -

--- CustomServiceRegistry.RegistryApi/Features/ServiceRegistry/Core/ServiceRegistryService.cs
+++ CustomServiceRegistry.RegistryApi/Features/ServiceRegistry/Core/ServiceRegistryService.cs
@@ -15,16 +15,25 @@ public class ServiceRegistryService : IServiceRegistryService
 
     public ServiceRegistryService()
     {
-        _centralRegistryCollection = CollectionNames.CentralRegistryCollection.GetCollection<CentralRegistryCollection>();
-        _serviceLogCollection = CollectionNames.ServiceLogCollection.GetCollection<ServiceLogCollection>();
+        _centralRegistryCollection =
+            CollectionNames.CentralRegistryCollection.GetCollection<CentralRegistryCollection>();
+        _serviceLogCollection =
+            CollectionNames.ServiceLogCollection.GetCollection<ServiceLogCollection>();
     }
 
-    public async Task<Result<RegisterServiceResponse>> RegisterServiceAsync(RegisterServiceCommand command, Guid tenantId, CancellationToken cs = default)
+    public async Task<Result<RegisterServiceResponse>> RegisterServiceAsync(
+        RegisterServiceCommand command,
+        Guid tenantId,
+        CancellationToken cs = default
+    )
     {
         Result<RegisterServiceResponse> result;
 
         var item = await _centralRegistryCollection
-            .Find(x => x.ServiceName.Trim().Equals(command.ServiceName.Trim(), StringComparison.CurrentCultureIgnoreCase))
+            .Find(x =>
+                x.ServiceName.Trim()
+                    .Equals(command.ServiceName.Trim(), StringComparison.CurrentCultureIgnoreCase)
+            )
             .FirstOrDefaultAsync(cancellationToken: cs);
 
         if (item is not null)
@@ -33,43 +42,58 @@ public class ServiceRegistryService : IServiceRegistryService
             goto result;
         }
 
-        await _centralRegistryCollection.InsertOneAsync(command.ToCollection(tenantId), cancellationToken: cs);
+        await _centralRegistryCollection.InsertOneAsync(
+            command.ToCollection(tenantId),
+            cancellationToken: cs
+        );
         result = Result<RegisterServiceResponse>.Success();
 
-    result:
+        result:
         return result;
     }
 
-    public async Task<Result<DeregisterServiceResponse>> DeregisterAsync(DeregisterServiceCommand command, Guid tenantId, CancellationToken cs = default)
+    public async Task<Result<DeregisterServiceResponse>> DeregisterAsync(
+        DeregisterServiceCommand command,
+        Guid tenantId,
+        CancellationToken cs = default
+    )
     {
         Result<DeregisterServiceResponse> result;
 
         var item = await _centralRegistryCollection
-            .Find(x => x.ServiceId == command.ServiceId && x.TenantId == tenantId).SingleOrDefaultAsync(cancellationToken: cs);
+            .Find(x => x.ServiceId == command.ServiceId && x.TenantId == tenantId)
+            .SingleOrDefaultAsync(cancellationToken: cs);
         if (item is null)
         {
             result = Result<DeregisterServiceResponse>.Fail("Service not found.");
             goto result;
         }
 
-        var filter = Builders<CentralRegistryCollection>.Filter.Eq(x => x.ServiceId, command.ServiceId);
+        var filter = Builders<CentralRegistryCollection>.Filter.Eq(
+            x => x.ServiceId,
+            command.ServiceId
+        );
         await _centralRegistryCollection.DeleteOneAsync(filter, cs);
 
-        var serviceLogsDeleteFilter = Builders<ServiceLogCollection>
-            .Filter
-            .Eq(x => x.ServiceInfo.ServiceId, item.ServiceId);
+        var serviceLogsDeleteFilter = Builders<ServiceLogCollection>.Filter.Eq(
+            x => x.ServiceInfo.ServiceId,
+            item.ServiceId
+        );
         await _serviceLogCollection.DeleteManyAsync(serviceLogsDeleteFilter, cs);
 
         result = Result<DeregisterServiceResponse>.Success();
 
-    result:
+        result:
         return result;
     }
 
     public async Task DeregisterAsync(Guid id, CancellationToken cs = default)
     {
-        var item = await _centralRegistryCollection
-            .Find(x => x.ServiceId == id).SingleOrDefaultAsync(cancellationToken: cs) ?? throw new Exception($"Service Id: {id} not found in the service registry.");
+        var item =
+            await _centralRegistryCollection
+                .Find(x => x.ServiceId == id)
+                .SingleOrDefaultAsync(cancellationToken: cs)
+            ?? throw new Exception($"Service Id: {id} not found in the service registry.");
 
         var filter = Builders<CentralRegistryCollection>.Filter.Eq(x => x.ServiceId, id);
         await _centralRegistryCollection.DeleteOneAsync(filter, cs);
