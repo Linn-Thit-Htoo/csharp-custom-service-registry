7124a7b625a2e524e650814a1e6d1da2f1a8c57d
Linn Thit Htoo <lth1212001@gmail.com>
2025-04-06 00:30:11 +0630

    feat: added ServiceDiscoveryService

--- CustomServiceRegistry.RegistryApi/Features/ServiceDiscovery/Core/IServiceDiscoveryService.cs
+++ CustomServiceRegistry.RegistryApi/Features/ServiceDiscovery/Core/IServiceDiscoveryService.cs
@@ -1,6 +1,10 @@
-﻿namespace CustomServiceRegistry.RegistryApi.Features.ServiceDiscovery.Core
+﻿using CustomServiceRegistry.RegistryApi.Features.ServiceDiscovery.DiscoverService;
+using CustomServiceRegistry.RegistryApi.Utils;
+
+namespace CustomServiceRegistry.RegistryApi.Features.ServiceDiscovery.Core
 {
     public interface IServiceDiscoveryService
     {
+        Task<Result<DiscoverServiceResponse>> DiscoverServiceAsync(Guid id, CancellationToken cs = default);
     }
 }
--- CustomServiceRegistry.RegistryApi/Features/ServiceDiscovery/Core/ServiceDiscoveryService.cs
+++ CustomServiceRegistry.RegistryApi/Features/ServiceDiscovery/Core/ServiceDiscoveryService.cs
@@ -1,6 +1,46 @@
-﻿namespace CustomServiceRegistry.RegistryApi.Features.ServiceDiscovery.Core
+﻿using CustomServiceRegistry.RegistryApi.Collections;
+using CustomServiceRegistry.RegistryApi.Constants;
+using CustomServiceRegistry.RegistryApi.Extensions;
+using CustomServiceRegistry.RegistryApi.Features.ServiceDiscovery.DiscoverService;
+using CustomServiceRegistry.RegistryApi.Utils;
+using MongoDB.Driver;
+
+namespace CustomServiceRegistry.RegistryApi.Features.ServiceDiscovery.Core
 {
     public class ServiceDiscoveryService : IServiceDiscoveryService
     {
+        private readonly IMongoCollection<CentralRegistryCollection> _centralRegistryCollection;
+
+        public ServiceDiscoveryService()
+        {
+            _centralRegistryCollection = CollectionNames.CentralRegistryCollection.GetCollection<CentralRegistryCollection>();
+        }
+
+        public async Task<Result<DiscoverServiceResponse>> DiscoverServiceAsync(Guid id, CancellationToken cs = default)
+        {
+            Result<DiscoverServiceResponse> result;
+
+            var item = await _centralRegistryCollection.Find(x => x.ServiceId == id)
+                .FirstOrDefaultAsync(cancellationToken: cs);
+            if (item is null)
+            {
+                result = Result<DiscoverServiceResponse>.NotFound("Service not found.");
+                goto result;
+            }
+
+            result = Result<DiscoverServiceResponse>.Success(new DiscoverServiceResponse
+            {
+                ServiceId = item.ServiceId,
+                HealthCheckUrl = item.HealthCheckUrl,
+                HostName = item.HostName,
+                Port = item.Port,
+                Scheme = item.Scheme,
+                ServiceName = item.ServiceName,
+                TenantId = item.TenantId
+            });
+
+        result:
+            return result;
+        }
     }
 }
