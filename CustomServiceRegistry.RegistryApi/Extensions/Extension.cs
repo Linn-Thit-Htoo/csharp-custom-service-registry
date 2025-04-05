2790e47585e819d80946e425a5c38a584a18d526
Linn Thit Htoo <lth1212001@gmail.com>
2025-04-05 12:24:30 +0630

    -

--- CustomServiceRegistry.RegistryApi/Extensions/Extension.cs
+++ CustomServiceRegistry.RegistryApi/Extensions/Extension.cs
@@ -5,6 +5,9 @@ namespace CustomServiceRegistry.RegistryApi.Extensions;
 public static class Extension
 {
     public static string ToJson(this object obj) => JsonSerializer.Serialize(obj);
+
     public static T ToObject<T>(this string jsonStr) => JsonSerializer.Deserialize<T>(jsonStr)!;
-    public static bool IsNullOrWhiteSpace(this string str) => string.IsNullOrWhiteSpace(str) || string.IsNullOrEmpty(str);
+
+    public static bool IsNullOrWhiteSpace(this string str) =>
+        string.IsNullOrWhiteSpace(str) || string.IsNullOrEmpty(str);
 }
