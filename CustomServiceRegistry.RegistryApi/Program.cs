50cc62ea391f5731dfaf14fddd9e094d6479c73e
Linn Thit Htoo <lth1212001@gmail.com>
2025-04-05 12:25:39 +0630

    -

--- CustomServiceRegistry.RegistryApi/Program.cs
+++ CustomServiceRegistry.RegistryApi/Program.cs
@@ -1,38 +1,37 @@
 using CustomServiceRegistry.RegistryApi.Extensions;
 
-namespace CustomServiceRegistry.RegistryApi
+namespace CustomServiceRegistry.RegistryApi;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddDependencies(builder);
+
+        var app = builder.Build();
+
+        app.UseResponseCompression();
+
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            builder.Services.AddDependencies(builder);
-
-            var app = builder.Build();
-
-            app.UseResponseCompression();
-
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseCors("CORSPolicy");
-
-            app.UseHealthChecks("/health");
-
-            app.UseAuthorization();
-
-            app.UseCheckApiKeyMiddleware();
-
-            app.MapControllers();
-
-            app.Run();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseCors("CORSPolicy");
+
+        app.UseHealthChecks("/health");
+
+        app.UseAuthorization();
+
+        app.UseCheckApiKeyMiddleware();
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
