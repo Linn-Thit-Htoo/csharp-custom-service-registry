ff84d15ecdf9896792d80b766cc232ba63949cbe
Linn Thit Htoo <lth1212001@gmail.com>
2025-04-05 12:23:33 +0630

    -

--- CustomServiceRegistry.RegistryApi/Collections/ServiceLogCollection.cs
+++ CustomServiceRegistry.RegistryApi/Collections/ServiceLogCollection.cs
@@ -1,23 +1,22 @@
 ﻿using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson;
 
-namespace CustomServiceRegistry.RegistryApi.Collections
+namespace CustomServiceRegistry.RegistryApi.Collections;
+
+public class ServiceLogCollection
 {
-    public class ServiceLogCollection
-    {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; }
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
 
-        [BsonRepresentation(BsonType.String)]
-        public Guid LogId { get; set; }
+    [BsonRepresentation(BsonType.String)]
+    public Guid LogId { get; set; }
 
-        [BsonRepresentation(BsonType.String)]
-        public Guid TenantId { get; set; }
+    [BsonRepresentation(BsonType.String)]
+    public Guid TenantId { get; set; }
 
-        public DateTime LogAt { get; set; }
-        public bool IsSuccess { get; set; }
-        public string? ErrorMessage { get; set; }
-        public CentralRegistryCollection ServiceInfo { get; set; }
-    }
+    public DateTime LogAt { get; set; }
+    public bool IsSuccess { get; set; }
+    public string? ErrorMessage { get; set; }
+    public CentralRegistryCollection ServiceInfo { get; set; }
 }
