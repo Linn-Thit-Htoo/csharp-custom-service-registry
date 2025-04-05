363c74c0d154b8a2381ec00175bb8fa9743ee21f
Linn Thit Htoo <lth1212001@gmail.com>
2025-04-05 12:25:21 +0630

    -

--- CustomServiceRegistry.RegistryApi/Services/ActiveHealthCheckBackgroundService.cs
+++ CustomServiceRegistry.RegistryApi/Services/ActiveHealthCheckBackgroundService.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Net;
+﻿using System.Net;
 using CustomServiceRegistry.RegistryApi.Collections;
 using CustomServiceRegistry.RegistryApi.Configurations;
 using CustomServiceRegistry.RegistryApi.Constants;
