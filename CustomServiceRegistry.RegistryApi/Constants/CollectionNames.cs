d088bfa980e0f36d4fb5d4846463f0b38031c983
Linn Thit Htoo <lth1212001@gmail.com>
2025-04-05 12:24:06 +0630

    -

--- CustomServiceRegistry.RegistryApi/Constants/CollectionNames.cs
+++ CustomServiceRegistry.RegistryApi/Constants/CollectionNames.cs
@@ -1,9 +1,8 @@
-﻿namespace CustomServiceRegistry.RegistryApi.Constants
+﻿namespace CustomServiceRegistry.RegistryApi.Constants;
+
+public class CollectionNames
 {
-    public class CollectionNames
-    {
-        public static string TenantCollection { get; } = "TenantCollection";
-        public static string CentralRegistryCollection { get; } = "CentralRegistryCollection";
-        public static string ServiceLogCollection { get; } = "ServiceLogCollection";
-    }
+    public static string TenantCollection { get; } = "TenantCollection";
+    public static string CentralRegistryCollection { get; } = "CentralRegistryCollection";
+    public static string ServiceLogCollection { get; } = "ServiceLogCollection";
 }
