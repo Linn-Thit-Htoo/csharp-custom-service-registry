f81514248654309f1d79d4d17927e1580d522a02
Linn Thit Htoo <lth1212001@gmail.com>
2025-04-06 00:09:58 +0630

    -

--- CustomServiceRegistry.RegistryApi/Features/ServiceLog/Core/ServiceLogService.cs
+++ CustomServiceRegistry.RegistryApi/Features/ServiceLog/Core/ServiceLogService.cs
@@ -11,10 +11,14 @@ public class ServiceLogService : IServiceLogService
 
     public ServiceLogService()
     {
-        _serviceLogCollection = CollectionNames.ServiceLogCollection.GetCollection<ServiceLogCollection>();
+        _serviceLogCollection =
+            CollectionNames.ServiceLogCollection.GetCollection<ServiceLogCollection>();
     }
 
-    public async Task<List<ServiceLogCollection>> GetLogCollectionAsync(Guid tenantId, CancellationToken cs = default)
+    public async Task<List<ServiceLogCollection>> GetLogCollectionAsync(
+        Guid tenantId,
+        CancellationToken cs = default
+    )
     {
         return await _serviceLogCollection.Find(x => true).ToListAsync(cancellationToken: cs);
     }
