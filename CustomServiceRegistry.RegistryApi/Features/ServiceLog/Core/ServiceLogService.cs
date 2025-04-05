fa2fb9f38971c98909e77ece46019dc97a82cc50
Linn Thit Htoo <lth1212001@gmail.com>
2025-04-06 00:09:54 +0630

    -

--- CustomServiceRegistry.RegistryApi/Features/ServiceLog/Core/ServiceLogService.cs
+++ CustomServiceRegistry.RegistryApi/Features/ServiceLog/Core/ServiceLogService.cs
@@ -3,20 +3,19 @@ using CustomServiceRegistry.RegistryApi.Constants;
 using CustomServiceRegistry.RegistryApi.Extensions;
 using MongoDB.Driver;
 
-namespace CustomServiceRegistry.RegistryApi.Features.ServiceLog.Core
+namespace CustomServiceRegistry.RegistryApi.Features.ServiceLog.Core;
+
+public class ServiceLogService : IServiceLogService
 {
-    public class ServiceLogService : IServiceLogService
+    private readonly IMongoCollection<ServiceLogCollection> _serviceLogCollection;
+
+    public ServiceLogService()
     {
-        private readonly IMongoCollection<ServiceLogCollection> _serviceLogCollection;
+        _serviceLogCollection = CollectionNames.ServiceLogCollection.GetCollection<ServiceLogCollection>();
+    }
 
-        public ServiceLogService()
-        {
-            _serviceLogCollection = CollectionNames.ServiceLogCollection.GetCollection<ServiceLogCollection>();
-        }
-
-        public async Task<List<ServiceLogCollection>> GetLogCollectionAsync(Guid tenantId, CancellationToken cs = default)
-        {
-            return await _serviceLogCollection.Find(x => true).ToListAsync(cancellationToken: cs);
-        }
+    public async Task<List<ServiceLogCollection>> GetLogCollectionAsync(Guid tenantId, CancellationToken cs = default)
+    {
+        return await _serviceLogCollection.Find(x => true).ToListAsync(cancellationToken: cs);
     }
 }
