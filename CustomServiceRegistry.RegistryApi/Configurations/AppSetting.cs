6da8d17646b53fe9bdeca190ee6d9b09e8e26f03
Linn Thit Htoo <lth1212001@gmail.com>
2025-04-05 12:23:50 +0630

    -

--- CustomServiceRegistry.RegistryApi/Configurations/AppSetting.cs
+++ CustomServiceRegistry.RegistryApi/Configurations/AppSetting.cs
@@ -1,25 +1,24 @@
-﻿namespace CustomServiceRegistry.RegistryApi.Configurations
+﻿namespace CustomServiceRegistry.RegistryApi.Configurations;
+
+public class AppSetting
 {
-    public class AppSetting
-    {
-        public Connectionstrings ConnectionStrings { get; set; }
-        public int RetryCount { get; set; }
-        public Logging Logging { get; set; }
-    }
+    public Connectionstrings ConnectionStrings { get; set; }
+    public int RetryCount { get; set; }
+    public Logging Logging { get; set; }
+}
 
-    public class Connectionstrings
-    {
-        public string MongoConnection { get; set; }
-    }
+public class Connectionstrings
+{
+    public string MongoConnection { get; set; }
+}
 
-    public class Logging
-    {
-        public Loglevel LogLevel { get; set; }
-    }
+public class Logging
+{
+    public Loglevel LogLevel { get; set; }
+}
 
-    public class Loglevel
-    {
-        public string Default { get; set; }
-        public string MicrosoftAspNetCore { get; set; }
-    }
+public class Loglevel
+{
+    public string Default { get; set; }
+    public string MicrosoftAspNetCore { get; set; }
 }
