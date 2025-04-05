a2665449d76352b58d67f5ff4b8d970ac5696454
Linn Thit Htoo <lth1212001@gmail.com>
2025-04-05 12:24:55 +0630

    -

--- CustomServiceRegistry.RegistryApi/Middlewares/CheckApiKeyMiddleware.cs
+++ CustomServiceRegistry.RegistryApi/Middlewares/CheckApiKeyMiddleware.cs
@@ -3,51 +3,50 @@ using CustomServiceRegistry.RegistryApi.Extensions;
 using CustomServiceRegistry.RegistryApi.Utils;
 using System.Net;
 
-namespace CustomServiceRegistry.RegistryApi.Middlewares
+namespace CustomServiceRegistry.RegistryApi.Middlewares;
+
+public class CheckApiKeyMiddleware : IMiddleware
 {
-    public class CheckApiKeyMiddleware : IMiddleware
+    private readonly ILogger<CheckApiKeyMiddleware> _logger;
+
+    public CheckApiKeyMiddleware(ILogger<CheckApiKeyMiddleware> logger)
     {
-        private readonly ILogger<CheckApiKeyMiddleware> _logger;
+        _logger = logger;
+    }
 
-        public CheckApiKeyMiddleware(ILogger<CheckApiKeyMiddleware> logger)
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        Result<object> result;
+        string requestPath = context.Request.Path;
+
+        if (GetWhiteListPaths().Any(x => requestPath.Equals(x)))
         {
-            _logger = logger;
-        }
-
-        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
-        {
-            Result<object> result;
-            string requestPath = context.Request.Path;
-
-            if (GetWhiteListPaths().Any(x => requestPath.Equals(x)))
-            {
-                await next(context);
-                return;
-            }
-
-            string apiKey = context.Request.Headers[ApplicationConstants.ApiKey].ToString();
-            if (apiKey is null || apiKey.IsNullOrWhiteSpace())
-            {
-                result = Result<object>.NotFound("Api Key not found.");
-
-                context.Response.StatusCode = (int)HttpStatusCode.OK;
-                context.Response.ContentType = "application/json";
-                await context.Response.WriteAsync(result.ToJson());
-
-                return;
-            }
-
             await next(context);
+            return;
         }
 
-        private List<string> GetWhiteListPaths()
+        string apiKey = context.Request.Headers[ApplicationConstants.ApiKey].ToString();
+        if (apiKey is null || apiKey.IsNullOrWhiteSpace())
         {
-            return new List<string>()
-            {
-                "/api/Tenant/SaveTenant",
-                "/api/Tenant/GetTenantInfoById",
-                "/api/ServiceLog/StreamLogs"
-            };
+            result = Result<object>.NotFound("Api Key not found.");
+
+            context.Response.StatusCode = (int)HttpStatusCode.OK;
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsync(result.ToJson());
+
+            return;
         }
+
+        await next(context);
+    }
+
+    private List<string> GetWhiteListPaths()
+    {
+        return new List<string>()
+        {
+            "/api/Tenant/SaveTenant",
+            "/api/Tenant/GetTenantInfoById",
+            "/api/ServiceLog/StreamLogs"
+        };
     }
 }
