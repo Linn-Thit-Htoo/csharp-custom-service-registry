5b9943b5b2cbd94bcf86c779b95ef59ff169cb1d
Linn Thit Htoo <lth1212001@gmail.com>
2025-04-05 12:24:47 +0630

    -

--- CustomServiceRegistry.RegistryApi/Extensions/MongoDbExtensions.cs
+++ CustomServiceRegistry.RegistryApi/Extensions/MongoDbExtensions.cs
@@ -6,9 +6,7 @@ public static class MongoDbExtensions
 {
     public static IMongoCollection<T> GetCollection<T>(this string collectionName)
     {
-        var client = new MongoClient(
-            "mongodb://localhost:27017"
-        );
+        var client = new MongoClient("mongodb://localhost:27017");
         var database = client.GetDatabase("ServiceRegistry");
         var collection = database.GetCollection<T>(collectionName);
 
