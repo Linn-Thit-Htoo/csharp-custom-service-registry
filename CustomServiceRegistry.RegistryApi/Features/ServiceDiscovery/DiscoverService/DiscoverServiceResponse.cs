0d009bc6570d099967a2600593b7ec175aafd032
Linn Thit Htoo <lth1212001@gmail.com>
2025-04-06 23:07:52 +0630

    -

--- CustomServiceRegistry.RegistryApi/Features/ServiceDiscovery/DiscoverService/DiscoverServiceResponse.cs
+++ CustomServiceRegistry.RegistryApi/Features/ServiceDiscovery/DiscoverService/DiscoverServiceResponse.cs
@@ -1,16 +1,15 @@
 ﻿using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson;
 
-namespace CustomServiceRegistry.RegistryApi.Features.ServiceDiscovery.DiscoverService
+namespace CustomServiceRegistry.RegistryApi.Features.ServiceDiscovery.DiscoverService;
+
+public class DiscoverServiceResponse
 {
-    public class DiscoverServiceResponse
-    {
-        public Guid ServiceId { get; set; }
-        public Guid TenantId { get; set; }
-        public string ServiceName { get; set; }
-        public string Scheme { get; set; }
-        public string HostName { get; set; }
-        public int Port { get; set; }
-        public string HealthCheckUrl { get; set; }
-    }
+    public Guid ServiceId { get; set; }
+    public Guid TenantId { get; set; }
+    public string ServiceName { get; set; }
+    public string Scheme { get; set; }
+    public string HostName { get; set; }
+    public int Port { get; set; }
+    public string HealthCheckUrl { get; set; }
 }
