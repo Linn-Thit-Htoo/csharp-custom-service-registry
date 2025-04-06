97e455e89ad1c702e33f84a28cc41f28311c01f3
Linn Thit Htoo <lth1212001@gmail.com>
2025-04-06 23:07:40 +0630

    -

--- CustomServiceRegistry.RegistryApi/Features/ServiceDiscovery/DiscoverService/DiscoverServiceQueryHandler.cs
+++ CustomServiceRegistry.RegistryApi/Features/ServiceDiscovery/DiscoverService/DiscoverServiceQueryHandler.cs
@@ -3,31 +3,30 @@ using CustomServiceRegistry.RegistryApi.Features.ServiceDiscovery.Core;
 using CustomServiceRegistry.RegistryApi.Utils;
 using MediatR;
 
-namespace CustomServiceRegistry.RegistryApi.Features.ServiceDiscovery.DiscoverService
+namespace CustomServiceRegistry.RegistryApi.Features.ServiceDiscovery.DiscoverService;
+
+public class DiscoverServiceQueryHandler : IRequestHandler<DiscoverServiceQuery, Result<DiscoverServiceResponse>>
 {
-    public class DiscoverServiceQueryHandler : IRequestHandler<DiscoverServiceQuery, Result<DiscoverServiceResponse>>
+    private readonly IServiceDiscoveryService _serviceDiscoveryService;
+
+    public DiscoverServiceQueryHandler(IServiceDiscoveryService serviceDiscoveryService)
     {
-        private readonly IServiceDiscoveryService _serviceDiscoveryService;
+        _serviceDiscoveryService = serviceDiscoveryService;
+    }
 
-        public DiscoverServiceQueryHandler(IServiceDiscoveryService serviceDiscoveryService)
+    public async Task<Result<DiscoverServiceResponse>> Handle(DiscoverServiceQuery request, CancellationToken cancellationToken)
+    {
+        Result<DiscoverServiceResponse> result;
+
+        if (request.ServiceName.IsNullOrWhiteSpace())
         {
-            _serviceDiscoveryService = serviceDiscoveryService;
+            result = Result<DiscoverServiceResponse>.Fail("Id is required.");
+            goto result;
         }
 
-        public async Task<Result<DiscoverServiceResponse>> Handle(DiscoverServiceQuery request, CancellationToken cancellationToken)
-        {
-            Result<DiscoverServiceResponse> result;
+        result = await _serviceDiscoveryService.DiscoverServiceAsync(request.ServiceName, cancellationToken);
 
-            if (request.ServiceName.IsNullOrWhiteSpace())
-            {
-                result = Result<DiscoverServiceResponse>.Fail("Id is required.");
-                goto result;
-            }
-
-            result = await _serviceDiscoveryService.DiscoverServiceAsync(request.ServiceName, cancellationToken);
-
-        result:
-            return result;
-        }
+    result:
+        return result;
     }
 }
