2b657f74c39564e5b044a532b807485497d1c04b
Linn Thit Htoo <lth1212001@gmail.com>
2025-04-06 23:07:33 +0630

    -

--- CustomServiceRegistry.RegistryApi/Features/ServiceDiscovery/DiscoverService/DiscoverServiceQuery.cs
+++ CustomServiceRegistry.RegistryApi/Features/ServiceDiscovery/DiscoverService/DiscoverServiceQuery.cs
@@ -1,7 +1,6 @@
 ﻿using CustomServiceRegistry.RegistryApi.Utils;
 using MediatR;
 
-namespace CustomServiceRegistry.RegistryApi.Features.ServiceDiscovery.DiscoverService
-{
-    public record DiscoverServiceQuery(string ServiceName) : IRequest<Result<DiscoverServiceResponse>>;
-}
+namespace CustomServiceRegistry.RegistryApi.Features.ServiceDiscovery.DiscoverService;
+
+public record DiscoverServiceQuery(string ServiceName) : IRequest<Result<DiscoverServiceResponse>>;
