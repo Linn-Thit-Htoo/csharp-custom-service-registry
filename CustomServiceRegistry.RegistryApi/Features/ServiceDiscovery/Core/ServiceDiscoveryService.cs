0c069654b8177eac2f273fd72289c15dd9b34944
Linn Thit Htoo <lth1212001@gmail.com>
2025-04-06 23:07:23 +0630

    -

--- CustomServiceRegistry.RegistryApi/Features/ServiceDiscovery/Core/ServiceDiscoveryService.cs
+++ CustomServiceRegistry.RegistryApi/Features/ServiceDiscovery/Core/ServiceDiscoveryService.cs
@@ -13,14 +13,19 @@ public class ServiceDiscoveryService : IServiceDiscoveryService
 
     public ServiceDiscoveryService()
     {
-        _centralRegistryCollection = CollectionNames.CentralRegistryCollection.GetCollection<CentralRegistryCollection>();
+        _centralRegistryCollection =
+            CollectionNames.CentralRegistryCollection.GetCollection<CentralRegistryCollection>();
     }
 
-    public async Task<Result<DiscoverServiceResponse>> DiscoverServiceAsync(string serviceName, CancellationToken cs = default)
+    public async Task<Result<DiscoverServiceResponse>> DiscoverServiceAsync(
+        string serviceName,
+        CancellationToken cs = default
+    )
     {
         Result<DiscoverServiceResponse> result;
 
-        var item = await _centralRegistryCollection.Find(x => x.ServiceName.ToLower().Equals(serviceName.ToLower()))
+        var item = await _centralRegistryCollection
+            .Find(x => x.ServiceName.ToLower().Equals(serviceName.ToLower()))
             .FirstOrDefaultAsync(cancellationToken: cs);
         if (item is null)
         {
@@ -28,18 +33,20 @@ public class ServiceDiscoveryService : IServiceDiscoveryService
             goto result;
         }
 
-        result = Result<DiscoverServiceResponse>.Success(new DiscoverServiceResponse
-        {
-            ServiceId = item.ServiceId,
-            HealthCheckUrl = item.HealthCheckUrl,
-            HostName = item.HostName,
-            Port = item.Port,
-            Scheme = item.Scheme,
-            ServiceName = item.ServiceName,
-            TenantId = item.TenantId
-        });
+        result = Result<DiscoverServiceResponse>.Success(
+            new DiscoverServiceResponse
+            {
+                ServiceId = item.ServiceId,
+                HealthCheckUrl = item.HealthCheckUrl,
+                HostName = item.HostName,
+                Port = item.Port,
+                Scheme = item.Scheme,
+                ServiceName = item.ServiceName,
+                TenantId = item.TenantId
+            }
+        );
 
-    result:
+        result:
         return result;
     }
 }
