7e78cb62e22f6db12ffae7d94539b7dda4c0ab66
Linn-Thit-Htoo <lth1212001@gmail.com>
2025-04-07 09:21:58 +0630

    feat: added docker and docker compose

--- CustomServiceRegistry.RegistryApi/Program.cs
+++ CustomServiceRegistry.RegistryApi/Program.cs
@@ -14,7 +14,7 @@ public class Program
 
         app.UseResponseCompression();
 
-        if (app.Environment.IsDevelopment())
+        if (!app.Environment.IsProduction())
         {
             app.UseSwagger();
             app.UseSwaggerUI();
