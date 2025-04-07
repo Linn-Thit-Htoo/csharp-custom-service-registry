29a3513ce66ffed6636ed6a29e30a9f635fdbeff
Linn-Thit-Htoo <lth1212001@gmail.com>
2025-04-07 10:13:15 +0630

    chore: get connection string based on the env

--- CustomServiceRegistry.RegistryApi/Extensions/MongoDbExtensions.cs
+++ CustomServiceRegistry.RegistryApi/Extensions/MongoDbExtensions.cs
@@ -6,7 +6,22 @@ public static class MongoDbExtensions
 {
     public static IMongoCollection<T> GetCollection<T>(this string collectionName)
     {
-        var client = new MongoClient("mongodb://localhost:27017");
+        string environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")!;
+        string connectionString = string.Empty;
+
+        if (!environmentName.IsNullOrWhiteSpace() && environmentName.Equals("Development"))
+        {
+            connectionString = "mongodb://localhost:27017";
+        }
+        else
+        {
+            connectionString = "mongodb://mongo:27017";
+        }
+
+        if (connectionString.IsNullOrWhiteSpace())
+            throw new ArgumentNullException("ConnectionString cannot be empty.");
+
+        var client = new MongoClient(connectionString);
         var database = client.GetDatabase("ServiceRegistry");
         var collection = database.GetCollection<T>(collectionName);
 
