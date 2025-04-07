129f7ef09fc6a1bc724b7bc0e26754638c82819a
Linn Thit Htoo <lth1212001@gmail.com>
2025-04-07 06:41:10 +0630

    -

--- CustomServiceRegistry.RegistryApi/Features/Tenant/Core/ITenantService.cs
+++ CustomServiceRegistry.RegistryApi/Features/Tenant/Core/ITenantService.cs
@@ -6,6 +6,12 @@ namespace CustomServiceRegistry.RegistryApi.Features.Tenant.Core;
 
 public interface ITenantService
 {
-    Task<Result<CreateTenantResponse>> CreateTenantAsync(CreateTenantCommand command, CancellationToken cs = default);
-    Task<Result<GetTenantByIdResponse>> GetTenantByIdAsync(string tenantId, CancellationToken cancellationToken = default);
+    Task<Result<CreateTenantResponse>> CreateTenantAsync(
+        CreateTenantCommand command,
+        CancellationToken cs = default
+    );
+    Task<Result<GetTenantByIdResponse>> GetTenantByIdAsync(
+        string tenantId,
+        CancellationToken cancellationToken = default
+    );
 }
