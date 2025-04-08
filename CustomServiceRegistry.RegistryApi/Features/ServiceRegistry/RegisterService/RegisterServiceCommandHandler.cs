a8b64fd8d3f58bbaf3f7d43d9da06bbe96db2ba2
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-08 07:29:01 +0630

    -

--- CustomServiceRegistry.RegistryApi/Features/ServiceRegistry/RegisterService/RegisterServiceCommandHandler.cs
+++ CustomServiceRegistry.RegistryApi/Features/ServiceRegistry/RegisterService/RegisterServiceCommandHandler.cs
@@ -3,25 +3,24 @@ using CustomServiceRegistry.RegistryApi.Features.ServiceRegistry.Core;
 using CustomServiceRegistry.RegistryApi.Utils;
 using MediatR;
 
-namespace CustomServiceRegistry.RegistryApi.Features.ServiceRegistry.RegisterService
+namespace CustomServiceRegistry.RegistryApi.Features.ServiceRegistry.RegisterService;
+
+public class RegisterServiceCommandHandler : IRequestHandler<RegisterServiceCommand, Result<RegisterServiceResponse>>
 {
-    public class RegisterServiceCommandHandler : IRequestHandler<RegisterServiceCommand, Result<RegisterServiceResponse>>
+    private readonly IServiceRegistryService _serviceRegistryService;
+    private readonly HttpContext _httpContext;
+
+    public RegisterServiceCommandHandler(IServiceRegistryService serviceRegistryService, IHttpContextAccessor httpContextAccessor)
     {
-        private readonly IServiceRegistryService _serviceRegistryService;
-        private readonly HttpContext _httpContext;
+        _serviceRegistryService = serviceRegistryService;
+        _httpContext = httpContextAccessor.HttpContext!;
+    }
 
-        public RegisterServiceCommandHandler(IServiceRegistryService serviceRegistryService, IHttpContextAccessor httpContextAccessor)
-        {
-            _serviceRegistryService = serviceRegistryService;
-            _httpContext = httpContextAccessor.HttpContext!;
-        }
+    public async Task<Result<RegisterServiceResponse>> Handle(RegisterServiceCommand request, CancellationToken cancellationToken)
+    {
+        string? apiKey = _httpContext.Request.Headers[ApplicationConstants.ApiKey].ToString()
+            ?? throw new Exception("Api Key cannot be nul.");
 
-        public async Task<Result<RegisterServiceResponse>> Handle(RegisterServiceCommand request, CancellationToken cancellationToken)
-        {
-            string? apiKey = _httpContext.Request.Headers[ApplicationConstants.ApiKey].ToString()
-                ?? throw new Exception("Api Key cannot be nul.");
-
-            return await _serviceRegistryService.RegisterServiceAsync(request, Guid.Parse(apiKey), cancellationToken);
-        }
+        return await _serviceRegistryService.RegisterServiceAsync(request, Guid.Parse(apiKey), cancellationToken);
     }
 }
