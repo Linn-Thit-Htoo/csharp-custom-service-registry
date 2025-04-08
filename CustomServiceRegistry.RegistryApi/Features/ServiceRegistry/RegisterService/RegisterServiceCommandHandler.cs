4c282f8c87d453b6dba726be6bdaaa1a895ccfe4
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-08 20:30:21 +0630

    -

--- CustomServiceRegistry.RegistryApi/Features/ServiceRegistry/RegisterService/RegisterServiceCommandHandler.cs
+++ CustomServiceRegistry.RegistryApi/Features/ServiceRegistry/RegisterService/RegisterServiceCommandHandler.cs
@@ -5,22 +5,34 @@ using MediatR;
 
 namespace CustomServiceRegistry.RegistryApi.Features.ServiceRegistry.RegisterService;
 
-public class RegisterServiceCommandHandler : IRequestHandler<RegisterServiceCommand, Result<RegisterServiceResponse>>
+public class RegisterServiceCommandHandler
+    : IRequestHandler<RegisterServiceCommand, Result<RegisterServiceResponse>>
 {
     private readonly IServiceRegistryService _serviceRegistryService;
     private readonly HttpContext _httpContext;
 
-    public RegisterServiceCommandHandler(IServiceRegistryService serviceRegistryService, IHttpContextAccessor httpContextAccessor)
+    public RegisterServiceCommandHandler(
+        IServiceRegistryService serviceRegistryService,
+        IHttpContextAccessor httpContextAccessor
+    )
     {
         _serviceRegistryService = serviceRegistryService;
         _httpContext = httpContextAccessor.HttpContext!;
     }
 
-    public async Task<Result<RegisterServiceResponse>> Handle(RegisterServiceCommand request, CancellationToken cancellationToken)
+    public async Task<Result<RegisterServiceResponse>> Handle(
+        RegisterServiceCommand request,
+        CancellationToken cancellationToken
+    )
     {
-        string? apiKey = _httpContext.Request.Headers[ApplicationConstants.ApiKey].ToString()
+        string? apiKey =
+            _httpContext.Request.Headers[ApplicationConstants.ApiKey].ToString()
             ?? throw new Exception("Api Key cannot be nul.");
 
-        return await _serviceRegistryService.RegisterServiceAsync(request, Guid.Parse(apiKey), cancellationToken);
+        return await _serviceRegistryService.RegisterServiceAsync(
+            request,
+            Guid.Parse(apiKey),
+            cancellationToken
+        );
     }
 }
