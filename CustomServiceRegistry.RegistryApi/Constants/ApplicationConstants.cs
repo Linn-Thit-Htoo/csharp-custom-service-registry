c6fd686b470dd900a3f67c083c6c4fa0fddf624b
Linn-Thit-Htoo <lth1212001@gmail.com>
2025-04-08 11:25:15 +0630

    -

--- CustomServiceRegistry.RegistryApi/Constants/ApplicationConstants.cs
+++ CustomServiceRegistry.RegistryApi/Constants/ApplicationConstants.cs
@@ -5,7 +5,8 @@ public class ApplicationConstants
     public static string ApiKey { get; } = "X-Key";
     public static string DatabaseName { get; } = "ServiceRegistry";
     public static string LocalMongoConnectionString { get; } = "mongodb://localhost:27017";
-    public static string ContainerizedMongoConnectionString { get; } = "mongodb://root:root@mongo:27017/?authSource=admin";
+    public static string ContainerizedMongoConnectionString { get; } =
+        "mongodb://root:root@mongo:27017/?authSource=admin";
     public static string Environment { get; } = "ASPNETCORE_ENVIRONMENT";
     public static string DevelopmentEnvironment { get; } = "Development";
     public static string ContentTypeJson { get; } = "application/json";
