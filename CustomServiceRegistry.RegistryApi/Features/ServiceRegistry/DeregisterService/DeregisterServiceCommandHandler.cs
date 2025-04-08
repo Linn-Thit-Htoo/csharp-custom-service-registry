07f50177cb34adacef07cbff292228f78333f778
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-08 00:19:23 -0700

    -

--- CustomServiceRegistry.RegistryApi/Features/ServiceRegistry/DeregisterService/DeregisterServiceCommandHandler.cs
+++ CustomServiceRegistry.RegistryApi/Features/ServiceRegistry/DeregisterService/DeregisterServiceCommandHandler.cs
@@ -3,25 +3,24 @@ using CustomServiceRegistry.RegistryApi.Features.ServiceRegistry.Core;
 using CustomServiceRegistry.RegistryApi.Utils;
 using MediatR;
 
-namespace CustomServiceRegistry.RegistryApi.Features.ServiceRegistry.DeregisterService
+namespace CustomServiceRegistry.RegistryApi.Features.ServiceRegistry.DeregisterService;
+
+public class DeregisterServiceCommandHandler : IRequestHandler<DeregisterServiceCommand, Result<DeregisterServiceResponse>>
 {
-    public class DeregisterServiceCommandHandler : IRequestHandler<DeregisterServiceCommand, Result<DeregisterServiceResponse>>
+    private readonly IServiceRegistryService _serviceRegistryService;
+    private readonly HttpContext _httpContext;
+
+    public DeregisterServiceCommandHandler(IServiceRegistryService serviceRegistryService, IHttpContextAccessor httpContextAccessor)
     {
-        private readonly IServiceRegistryService _serviceRegistryService;
-        private readonly HttpContext _httpContext;
+        _serviceRegistryService = serviceRegistryService;
+        _httpContext = httpContextAccessor.HttpContext!;
+    }
 
-        public DeregisterServiceCommandHandler(IServiceRegistryService serviceRegistryService, IHttpContextAccessor httpContextAccessor)
-        {
-            _serviceRegistryService = serviceRegistryService;
-            _httpContext = httpContextAccessor.HttpContext!;
-        }
+    public async Task<Result<DeregisterServiceResponse>> Handle(DeregisterServiceCommand request, CancellationToken cancellationToken)
+    {
+        string? apiKey = _httpContext.Request.Headers[ApplicationConstants.ApiKey].ToString()
+            ?? throw new Exception("Api Key cannot be nul.");
 
-        public async Task<Result<DeregisterServiceResponse>> Handle(DeregisterServiceCommand request, CancellationToken cancellationToken)
-        {
-            string? apiKey = _httpContext.Request.Headers[ApplicationConstants.ApiKey].ToString()
-                ?? throw new Exception("Api Key cannot be nul.");
-
-            return await _serviceRegistryService.DeregisterAsync(request, Guid.Parse(apiKey), cancellationToken);
-        }
+        return await _serviceRegistryService.DeregisterAsync(request, Guid.Parse(apiKey), cancellationToken);
     }
 }
