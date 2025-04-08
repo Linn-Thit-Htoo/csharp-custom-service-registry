861e724dc0bda3fbf7388d6f76679146acd30d6f
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-08 07:29:19 +0630

    -

--- CustomServiceRegistry.RegistryApi/Features/Tenant/Core/TenantController.cs
+++ CustomServiceRegistry.RegistryApi/Features/Tenant/Core/TenantController.cs
@@ -5,37 +5,36 @@ using CustomServiceRegistry.RegistryApi.Features.Tenant.GetTenantById;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CustomServiceRegistry.RegistryApi.Features.Tenant.Core
+namespace CustomServiceRegistry.RegistryApi.Features.Tenant.Core;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TenantController : BaseController
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class TenantController : BaseController
+    private readonly ISender _sender;
+    private readonly HttpContext _context;
+
+    public TenantController(ISender sender, IHttpContextAccessor httpContextAccessor)
     {
-        private readonly ISender _sender;
-        private readonly HttpContext _context;
+        _sender = sender;
+        _context = httpContextAccessor.HttpContext!;
+    }
 
-        public TenantController(ISender sender, IHttpContextAccessor httpContextAccessor)
-        {
-            _sender = sender;
-            _context = httpContextAccessor.HttpContext!;
-        }
+    [HttpPost("SaveTenant")]
+    public async Task<IActionResult> SaveTenant(CreateTenantCommand command, CancellationToken cs)
+    {
+        var result = await _sender.Send(command, cs);
+        return Content(result);
+    }
 
-        [HttpPost("SaveTenant")]
-        public async Task<IActionResult> SaveTenant(CreateTenantCommand command, CancellationToken cs)
-        {
-            var result = await _sender.Send(command, cs);
-            return Content(result);
-        }
+    [HttpGet("GetTenantInfoById")]
+    public async Task<IActionResult> GetTenantById(CancellationToken cs)
+    {
+        string apiKey = _context.Request.Headers[ApplicationConstants.ApiKey]!;
 
-        [HttpGet("GetTenantInfoById")]
-        public async Task<IActionResult> GetTenantById(CancellationToken cs)
-        {
-            string apiKey = _context.Request.Headers[ApplicationConstants.ApiKey]!;
+        var query = new GetTenantByIdQuery(apiKey);
+        var result = await _sender.Send(query, cs);
 
-            var query = new GetTenantByIdQuery(apiKey);
-            var result = await _sender.Send(query, cs);
-
-            return Content(result);
-        }
+        return Content(result);
     }
 }
