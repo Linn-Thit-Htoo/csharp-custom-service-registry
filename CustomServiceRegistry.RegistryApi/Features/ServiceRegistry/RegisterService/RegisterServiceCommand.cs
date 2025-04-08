44d26460d4e713e0d51be09b35138baeabb87ced
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-08 07:28:56 +0630

    -

--- CustomServiceRegistry.RegistryApi/Features/ServiceRegistry/RegisterService/RegisterServiceCommand.cs
+++ CustomServiceRegistry.RegistryApi/Features/ServiceRegistry/RegisterService/RegisterServiceCommand.cs
@@ -1,14 +1,13 @@
 ﻿using CustomServiceRegistry.RegistryApi.Utils;
 using MediatR;
 
-namespace CustomServiceRegistry.RegistryApi.Features.ServiceRegistry.RegisterService
+namespace CustomServiceRegistry.RegistryApi.Features.ServiceRegistry.RegisterService;
+
+public class RegisterServiceCommand : IRequest<Result<RegisterServiceResponse>>
 {
-    public class RegisterServiceCommand : IRequest<Result<RegisterServiceResponse>>
-    {
-        public string ServiceName { get; set; }
-        public string Scheme { get; set; }
-        public string HostName { get; set; }
-        public int Port { get; set; }
-        public string HealthCheckUrl { get; set; }
-    }
+    public string ServiceName { get; set; }
+    public string Scheme { get; set; }
+    public string HostName { get; set; }
+    public int Port { get; set; }
+    public string HealthCheckUrl { get; set; }
 }
