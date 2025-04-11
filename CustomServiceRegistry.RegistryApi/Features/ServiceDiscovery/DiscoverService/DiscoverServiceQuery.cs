f6f91a6891d99b73aa9a23df38b975efae6508c8
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-11 06:45:18 +0630

    -

--- CustomServiceRegistry.RegistryApi/Features/ServiceDiscovery/DiscoverService/DiscoverServiceQuery.cs
+++ CustomServiceRegistry.RegistryApi/Features/ServiceDiscovery/DiscoverService/DiscoverServiceQuery.cs
@@ -1,4 +1,3 @@
-﻿
-namespace CustomServiceRegistry.RegistryApi.Features.ServiceDiscovery.DiscoverService;
+﻿namespace CustomServiceRegistry.RegistryApi.Features.ServiceDiscovery.DiscoverService;
 
 public record DiscoverServiceQuery(string ServiceName) : IRequest<Result<DiscoverServiceResponse>>;
