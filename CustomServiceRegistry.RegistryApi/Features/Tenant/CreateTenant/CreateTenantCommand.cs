f67cb986fb475e3bafd66b8d3c28f2766bb6dafd
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-14 06:35:24 +0630

    -

--- CustomServiceRegistry.RegistryApi/Features/Tenant/CreateTenant/CreateTenantCommand.cs
+++ CustomServiceRegistry.RegistryApi/Features/Tenant/CreateTenant/CreateTenantCommand.cs
@@ -1,7 +1,3 @@
-﻿using System.ComponentModel.DataAnnotations;
-using CustomServiceRegistry.RegistryApi.Utils;
-using MediatR;
-
-namespace CustomServiceRegistry.RegistryApi.Features.Tenant.CreateTenant;
+﻿namespace CustomServiceRegistry.RegistryApi.Features.Tenant.CreateTenant;
 
 public record CreateTenantCommand(string ApplicationName) : IRequest<Result<CreateTenantResponse>>;
